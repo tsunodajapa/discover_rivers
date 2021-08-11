83eca6831427adbe494845bdfac1cd701601c7af
tsunodagabriel <gabriel.tsunoda@vitta.me>
2021-08-11 00:58:41 -0300

    feat: get rivers and sorted them

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
     Teste
+    <button @click="handleOpenModal">Teste</button>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component, Provide, Vue } from 'nuxt-property-decorator';
+import { RiversService, River } from '../services/RiversService';
 
-export default Vue.extend({})
+@Component({
+  async asyncData() {
+    const rivers = await new RiversService().index();
+
+    return { rivers }
+  }
+})
+export default class Home extends Vue{
+
+  rivers: River[] = [];
+
+  get riversSorted() {
+    if (!this.rivers || !this.rivers.length) return [];
+
+    const riversWithLengthFormated =
+      this.rivers.map(river => ({...river, length: +river.length.replace(/(km|,)/g, '')}));
+
+    return riversWithLengthFormated.sort((a, b) => {
+      if (a.length > b.length) return -1;
+      if (a.length < b.length) return 1;
+      return 0;
+    });
+  };
+
+  async handleOpenModal() {
+    console.log(this.riversSorted);
+  };
+
+}
 </script>
